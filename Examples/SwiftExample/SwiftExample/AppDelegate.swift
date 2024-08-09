ad844ac75ee522a83749590d9aa638830cfb83cf
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2024-08-09 10:55:51 +0100

    Remove scene

--- Examples/SwiftExample/SwiftExample/AppDelegate.swift
+++ Examples/SwiftExample/SwiftExample/AppDelegate.swift
@@ -10,25 +10,25 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Create a new window instance and make it the key window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // Set the initial view controller
+        let initialViewController = WriteKeyVC() // Replace with your initial view controller
+        
+        // Embed in a navigation controller if needed
+        let navigationController = UINavigationController(rootViewController: initialViewController)
+        
+        // Set the root view controller and make the window visible
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
